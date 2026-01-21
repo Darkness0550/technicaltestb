96b14373847dc0e2cf8061d68965f8da3e0b2d5b
Darkness0550 <jeanpaulchilonsalazar@gmail.com>
2026-01-20 21:20:06 -0500

    product.crud.complete

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { PrismaService } from '../prisma.service';
 @Injectable()
 export class ProductsService {
+  private logger = new Logger(ProductsService.name);
+
+  constructor(private prisma: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    this.logger.log(
+      `Creating product with data: ${JSON.stringify(createProductDto)}`,
+    );
+    return this.prisma.product.create({
+      data: createProductDto,
+    });
   }
 
   findAll() {
-    return `This action returns all products`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.prisma.product.findMany({
+      orderBy: { id: 'asc' },
+    });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    return this.prisma.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    return this.prisma.product.delete({
+      where: { id },
+    });
   }
 }
