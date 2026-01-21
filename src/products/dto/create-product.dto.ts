8219976f46737e442af03940e182803f5a667488
Darkness0550 <jeanpaulchilonsalazar@gmail.com>
2026-01-20 20:32:58 -0500

    dto.product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  name: string;
+
+  @IsNumber()
+  @IsPositive({ message: 'Ingresa un precio positivo' })
+  unitPrice: number;
+}
